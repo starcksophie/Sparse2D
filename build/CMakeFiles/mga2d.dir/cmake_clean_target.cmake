1de8f82aee3581a87d526cf21bb6b7d6b5e1f655
Samuel Farrens <samuel.farrens@gmail.com>
2017-11-23 11:54:48 +0100

    removed build files

--- build/CMakeFiles/mga2d.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmga2d.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sfarrens/Dropbox/CEA/P-ISAP/isap/cxx/sparse2d_opt_w_unit
-# Build directory: /Users/sfarrens/Dropbox/CEA/P-ISAP/isap/cxx/sparse2d_opt_w_unit/build2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mr_transform_test "mr_transform_test")
-add_test(example "example")
